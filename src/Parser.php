ce7d8a15fcbd2aabcb10a7e5c0bd00b7ae8d66c9
Artem Bagaev <ab02111996@yandex.ru>
2018-11-07 10:25:04 +0700

    Update Parser.php
    I tested this code on my JSON files and it works uncorrect with non-latin characters. The thing is standart PHP function strlen() returns wrong result when input string has russian letters. So I wrote my own function getLengthOfString() that get every letter in input string and returns count of letters.
    PHP Version - 7.1

--- src/Parser.php
+++ src/Parser.php
@@ -1,13 +1,9 @@
 <?php
-
 declare(strict_types=1);
-
 namespace JsonStreamingParser;
-
 use JsonStreamingParser\Exception\ParsingException;
 use JsonStreamingParser\Listener\ListenerInterface;
 use JsonStreamingParser\Listener\PositionAwareInterface;
-
 class Parser
 {
     const STATE_START_DOCUMENT = 0;
@@ -26,91 +22,73 @@ class Parser
     const STATE_IN_NULL = 11;
     const STATE_AFTER_VALUE = 12;
     const STATE_UNICODE_SURROGATE = 13;
-
     const STACK_OBJECT = 0;
     const STACK_ARRAY = 1;
     const STACK_KEY = 2;
     const STACK_STRING = 3;
-
     const UTF8_BOM = 1;
     const UTF16_BOM = 2;
     const UTF32_BOM = 3;
-
     /**
      * @var int
      */
     private $state;
-
     /**
      * @var int[]
      */
     private $stack = [];
-
     /**
      * @var resource
      */
     private $stream;
-
     /**
      * @var ListenerInterface
      */
     private $listener;
-
     /**
      * @var bool
      */
     private $emitWhitespace;
-
     /**
      * @var string
      */
     private $buffer = '';
-
     /**
      * @var int
      */
     private $bufferSize;
-
     /**
      * @var string[]
      */
     private $unicodeBuffer = [];
-
     /**
      * @var int
      */
     private $unicodeHighSurrogate = -1;
-
     /**
      * @var string
      */
     private $unicodeEscapeBuffer = '';
-
     /**
      * @var string
      */
     private $lineEnding;
-
     /**
      * @var int
      */
     private $lineNumber;
-
     /**
      * @var int
      */
     private $charNumber;
-
     /**
      * @var bool
      */
     private $stopParsing = false;
-
     /**
      * @var int
      */
     private $utfBom = 0;
-
     /**
      * @param resource $stream
      */
@@ -119,7 +97,6 @@ class Parser
         if (!\is_resource($stream) || 'stream' !== get_resource_type($stream)) {
             throw new \InvalidArgumentException('Invalid stream provided');
         }
-
         $this->stream = $stream;
         $this->listener = $listener;
         $this->emitWhitespace = $emitWhitespace;
@@ -127,57 +104,51 @@ class Parser
         $this->bufferSize = $bufferSize;
         $this->lineEnding = $lineEnding;
     }
-
     public function parse(): void
     {
         $this->lineNumber = 1;
         $this->charNumber = 1;
         $eof = false;
-
         while (!feof($this->stream) && !$eof) {
             $pos = ftell($this->stream);
             $line = stream_get_line($this->stream, $this->bufferSize, $this->lineEnding);
-
+            $len = $this->getLengthOfString($line);
             if (false === $line) {
                 $line = '';
             }
-
-            $ended = (bool) (ftell($this->stream) - \strlen($line) - $pos);
+            if ($line == "")
+                continue;
+            $ended = (bool) (ftell($this->stream) - $this->getLengthOfString($line) - $pos);
             // if we're still at the same place after stream_get_line, we're done
             $eof = ftell($this->stream) === $pos;
-
-            $byteLen = \strlen($line);
-            for ($i = 0; $i < $byteLen; ++$i) {
+            
+            for ($i = 0; $i < $len; ++$i) {
                 if ($this->listener instanceof PositionAwareInterface) {
                     $this->listener->setFilePosition($this->lineNumber, $this->charNumber);
                 }
-                $this->consumeChar($line[$i]);
+                $char = $line[$i];
+                $this->consumeChar($char);
                 ++$this->charNumber;
-
                 if ($this->stopParsing) {
                     return;
                 }
             }
-
             if ($ended) {
                 ++$this->lineNumber;
                 $this->charNumber = 1;
             }
         }
     }
-
     public function stop(): void
     {
         $this->stopParsing = true;
     }
-
     private function consumeChar(string $char): void
     {
         // see https://en.wikipedia.org/wiki/Byte_order_mark
         if ($this->charNumber < 5 && 1 === $this->lineNumber && $this->checkAndSkipUtfBom($char)) {
             return;
         }
-
         // valid whitespace characters in JSON (from RFC4627 for JSON) include:
         // space, horizontal tab, line feed or new line, and carriage return.
         // thanks: http://stackoverflow.com/questions/16042274/definition-of-whitespace-in-json
@@ -192,10 +163,8 @@ class Parser
             if ($this->emitWhitespace) {
                 $this->listener->whitespace($char);
             }
-
             return;
         }
-
         switch ($this->state) {
             case self::STATE_IN_STRING:
                 if ('"' === $char) {
@@ -208,7 +177,6 @@ class Parser
                     $this->buffer .= $char;
                 }
                 break;
-
             case self::STATE_IN_ARRAY:
                 if (']' === $char) {
                     $this->endArray();
@@ -216,7 +184,6 @@ class Parser
                     $this->startValue($char);
                 }
                 break;
-
             case self::STATE_IN_OBJECT:
                 if ('}' === $char) {
                     $this->endObject();
@@ -226,33 +193,27 @@ class Parser
                     $this->throwParseError('Start of string expected for object key. Instead got: '.$char);
                 }
                 break;
-
             case self::STATE_END_KEY:
                 if (':' !== $char) {
                     $this->throwParseError("Expected ':' after key.");
                 }
                 $this->state = self::STATE_AFTER_KEY;
                 break;
-
             case self::STATE_AFTER_KEY:
                 $this->startValue($char);
                 break;
-
             case self::STATE_START_ESCAPE:
                 $this->processEscapeCharacter($char);
                 break;
-
             case self::STATE_UNICODE:
                 $this->processUnicodeCharacter($char);
                 break;
-
             case self::STATE_UNICODE_SURROGATE:
                 $this->unicodeEscapeBuffer .= $char;
                 if (2 === mb_strlen($this->unicodeEscapeBuffer)) {
                     $this->endUnicodeSurrogateInterstitial();
                 }
                 break;
-
             case self::STATE_AFTER_VALUE:
                 $within = end($this->stack);
                 if (self::STACK_OBJECT === $within) {
@@ -277,7 +238,6 @@ class Parser
                     );
                 }
                 break;
-
             case self::STATE_IN_NUMBER:
                 if (ctype_digit($char)) {
                     $this->buffer .= $char;
@@ -305,28 +265,24 @@ class Parser
                     $this->consumeChar($char);
                 }
                 break;
-
             case self::STATE_IN_TRUE:
                 $this->buffer .= $char;
                 if (4 === \strlen($this->buffer)) {
                     $this->endTrue();
                 }
                 break;
-
             case self::STATE_IN_FALSE:
                 $this->buffer .= $char;
                 if (5 === \strlen($this->buffer)) {
                     $this->endFalse();
                 }
                 break;
-
             case self::STATE_IN_NULL:
                 $this->buffer .= $char;
                 if (4 === \strlen($this->buffer)) {
                     $this->endNull();
                 }
                 break;
-
             case self::STATE_START_DOCUMENT:
                 $this->listener->startDocument();
                 if ('[' === $char) {
@@ -337,7 +293,6 @@ class Parser
                     $this->throwParseError('Document must start with object or array.');
                 }
                 break;
-
             case self::STATE_END_DOCUMENT:
                 if ('[' !== $char && '{' !== $char) {
                     $this->throwParseError('Expected end of document.');
@@ -345,17 +300,14 @@ class Parser
                 $this->state = self::STATE_START_DOCUMENT;
                 $this->consumeChar($char);
                 break;
-
             case self::STATE_DONE:
                 $this->throwParseError('Expected end of document.');
                 break;
-
             default:
                 $this->throwParseError('Internal error. Reached an unknown state: '.$this->state);
                 break;
         }
     }
-
     private function checkAndSkipUtfBom(string $c): bool
     {
         if (1 === $this->charNumber) {
@@ -369,12 +321,10 @@ class Parser
                 $this->utfBom = self::UTF32_BOM;
             }
         }
-
         if (self::UTF16_BOM === $this->utfBom && 2 === $this->charNumber &&
               $c === \chr(254)) {
             $this->utfBom = self::UTF32_BOM;
         }
-
         if (self::UTF8_BOM === $this->utfBom && $this->charNumber < 4) {
             // UTF-8 BOM starts with chr(239) . chr(187) . chr(191)
             return true;
@@ -385,10 +335,8 @@ class Parser
         if (self::UTF32_BOM === $this->utfBom && $this->charNumber < 5) {
             return true;
         }
-
         return false;
     }
-
     /**
      * @throws ParsingException
      */
@@ -415,14 +363,12 @@ class Parser
             $this->throwParseError('Unexpected character for value: '.$c);
         }
     }
-
     private function startArray(): void
     {
         $this->listener->startArray();
         $this->state = self::STATE_IN_ARRAY;
         $this->stack[] = self::STACK_ARRAY;
     }
-
     private function endArray(): void
     {
         $popped = array_pop($this->stack);
@@ -431,19 +377,16 @@ class Parser
         }
         $this->listener->endArray();
         $this->state = self::STATE_AFTER_VALUE;
-
         if (empty($this->stack)) {
             $this->endDocument();
         }
     }
-
     private function startObject(): void
     {
         $this->listener->startObject();
         $this->state = self::STATE_IN_OBJECT;
         $this->stack[] = self::STACK_OBJECT;
     }
-
     private function endObject(): void
     {
         $popped = array_pop($this->stack);
@@ -452,24 +395,20 @@ class Parser
         }
         $this->listener->endObject();
         $this->state = self::STATE_AFTER_VALUE;
-
         if (empty($this->stack)) {
             $this->endDocument();
         }
     }
-
     private function startString(): void
     {
         $this->stack[] = self::STACK_STRING;
         $this->state = self::STATE_IN_STRING;
     }
-
     private function startKey(): void
     {
         $this->stack[] = self::STACK_KEY;
         $this->state = self::STATE_IN_STRING;
     }
-
     private function endString(): void
     {
         $popped = array_pop($this->stack);
@@ -484,7 +423,6 @@ class Parser
         }
         $this->buffer = '';
     }
-
     /**
      * @throws ParsingException
      */
@@ -511,12 +449,10 @@ class Parser
         } else {
             $this->throwParseError('Expected escaped character after backslash. Got: '.$c);
         }
-
         if (self::STATE_UNICODE !== $this->state) {
             $this->state = self::STATE_IN_STRING;
         }
     }
-
     /**
      * @throws ParsingException
      */
@@ -531,7 +467,6 @@ class Parser
         $this->unicodeBuffer[] = $char;
         if (4 === \count($this->unicodeBuffer)) {
             $codepoint = hexdec(implode('', $this->unicodeBuffer));
-
             if ($codepoint >= 0xD800 && $codepoint < 0xDC00) {
                 $this->unicodeHighSurrogate = $codepoint;
                 $this->unicodeBuffer = [];
@@ -541,7 +476,6 @@ class Parser
                     $this->throwParseError('Missing high surrogate for Unicode low surrogate.');
                 }
                 $combinedCodepoint = (($this->unicodeHighSurrogate - 0xD800) * 0x400) + ($codepoint - 0xDC00) + 0x10000;
-
                 $this->endUnicodeCharacter($combinedCodepoint);
             } else {
                 if (-1 !== $this->unicodeHighSurrogate) {
@@ -552,7 +486,6 @@ class Parser
             }
         }
     }
-
     private function endUnicodeSurrogateInterstitial(): void
     {
         $unicodeEscape = $this->unicodeEscapeBuffer;
@@ -562,7 +495,6 @@ class Parser
         $this->unicodeEscapeBuffer = '';
         $this->state = self::STATE_UNICODE;
     }
-
     private function endUnicodeCharacter(int $codepoint): void
     {
         $this->buffer .= ParserHelper::convertCodepointToCharacter($codepoint);
@@ -570,35 +502,29 @@ class Parser
         $this->unicodeHighSurrogate = -1;
         $this->state = self::STATE_IN_STRING;
     }
-
     private function startNumber(string $c): void
     {
         $this->state = self::STATE_IN_NUMBER;
         $this->buffer .= $c;
     }
-
     private function endNumber(): void
     {
         $this->listener->value(ParserHelper::convertToNumber($this->buffer));
         $this->buffer = '';
         $this->state = self::STATE_AFTER_VALUE;
     }
-
     private function endTrue(): void
     {
         $this->endSpecialValue(true, 'true');
     }
-
     private function endFalse(): void
     {
         $this->endSpecialValue(false, 'false');
     }
-
     private function endNull(): void
     {
         $this->endSpecialValue(null, 'null');
     }
-
     private function endSpecialValue($value, string $stringValue): void
     {
         if ($this->buffer === $stringValue) {
@@ -609,18 +535,24 @@ class Parser
         $this->buffer = '';
         $this->state = self::STATE_AFTER_VALUE;
     }
-
     private function endDocument(): void
     {
         $this->listener->endDocument();
         $this->state = self::STATE_END_DOCUMENT;
     }
-
     /**
      * @throws ParsingException
      */
     private function throwParseError(string $message): void
     {
         throw new ParsingException($this->lineNumber, $this->charNumber, $message);
+    }
+    
+    private function getLengthOfString(string $s) {
+        $chars = str_split($s);
+        $len = 0;
+        foreach ($chars as $c)
+            $len++;
+        return $len;
     }
 }
