3ec8956b4f88aed2d273a16c92527312ff557d75
NigelR <nigelrel3@yahoo.co.uk>
2019-06-17 17:53:06 +0100

    Add new regex listener.

---
+++ src/Listener/RegexListener.php
@@ -0,0 +1,268 @@
+<?php
+declare(strict_types=1);
+
+namespace JsonStreamingParser\Listener;
+
+/**
+ * Allow to select elements from a JSON being loaded via a regex expression.
+ * As the JSON is loaded you can use expressions such as /name to select
+ * a name attribute from the root of the document.  
+ * For arrays, /1/name which is the name element from the second element 
+ * of an array. Using things like /\d* will match any array element.
+ * 
+ * Basic usage (with file tests/data/example.json):
+ * $listener = new RegexListener(["/1/name" => function ($data ) {
+ *     echo "/1/name=".$data.PHP_EOL;
+ * }]);
+ * $parser = new Parser(fopen($filename, 'rb'), $listener);
+ * $parser->parse();
+ * 
+ * Capture groups can also be used to provide the element path to the data
+ * located, so (/\d*) will pass the array element number along with the
+ * element value.
+ * 
+ * Example using capture groups:
+ * $listener = new RegexListener(["(/\d*)" => function ($data, $path ) {
+ *     echo $path."=".$data['name'].PHP_EOL;
+ * }]);
+ * $parser = new Parser(fopen($filename, 'rb'), $listener);
+ * $parser->parse();
+ * 
+ * It's also possible to stop the load if you only need specific data.
+ * 
+ * Example to stop read (with file tests/data/ratherBig.json):
+ * $listener = new RegexListener();
+ * $parser = new Parser(fopen($filename, 'rb'), $listener);
+ * $listener->setMatch(["/total_rows" => function ($data ) use ($parser) {
+ *     echo "/total_rows=".$data.PHP_EOL;
+ *     $parser->stop();
+ * }]);
+ * 
+ * @author NigelRen <nigelrel3@yahoo.co.uk>
+ * 
+ */
+
+class RegexListener implements ListenerInterface  {
+    /**
+     * Constant to flag that it is an object level.
+     * @var int
+     */
+    const OBJECT = -1;
+    /**
+     * Constant to flag that it is an array level.
+     * @var int
+     */
+    const ARRAY = 0;
+    /**
+     * Various matching criteria along with the callback to execute.
+     * @var callable[] - [regex => callable, ...]
+     */
+    private $dataMatch = null;
+    /**
+     * Path to the current data item, used to match with $dataMatch for extract.
+     * Path is made up of keys for each level of structure.
+     * @var array string
+     */
+    private $path = [];
+
+    /**
+     * For arrays, this will have the numerical index, for objects this
+     * will be -1.
+     * @var array
+     */
+    private $pathIndex = [];
+
+    /**
+     * The last index of the path arrays.
+     * @var integer
+     */
+    private $pathLength = 0;
+    
+    /**
+     * Stack of the data structure so far encountered.
+     * Note that this will only contain data of interest.
+     * @var array
+     */
+    private $stack = [];
+    
+    /**
+     * The 'current' label.
+     * @var string|null
+     */
+    private $label;
+    
+    /**
+     * The start level at which data is needed for matching.  When an expression
+     * matches the current path this is set to pathLength.  When reducing the
+     * level of the data it will be reset then pathLength < levelInterest.
+     * @var int
+     */
+    private $levelInterest = PHP_INT_MAX;
+    
+    /**
+     * Flag set when endDocument is called.
+     * @var boolean
+     */
+    private $complete = false;
+    
+    /**
+     * Flag to indicate if an object has been encountered.
+     * Only really used when subsequent values are encountered in an array to
+     * ensure the array index is incremented.
+     * @var boolean
+     */
+    private $objectEncountered = false;
+    
+    /**
+     * @param array $dataMatch - [regex => closure()] format.
+     */
+    public function __construct(array $dataMatch = [])   {
+        $this->setMatch( $dataMatch );
+    }
+        
+    /**
+     * Sets the combination of regex and closures to be processed
+     * against the JSON input.
+     * Sent as [regex => closure()] format.
+     * @param array $dataMatch
+     */
+    public function setMatch (array $dataMatch): void   {
+        $this->dataMatch = $dataMatch;
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \JsonStreamingParser\Listener\ListenerInterface::startObject()
+     * @param int $index - set to 0 for arrays (used for index number).
+     */
+    public function startObject( $index = RegexListener::OBJECT ): void   {
+        // Does the hierarchy need updating?
+        if ( $index != RegexListener::OBJECT || $this->pathLength != 0 || !empty($this->label))    {
+            $this->path[] = $this->label ?? "";
+            $this->pathIndex[] = $index;
+            $this->pathLength++;
+        }
+        
+        $this->stack[] = [];
+        $this->label = null;
+        // If not currently interested in the data, check if it is now.
+        if ( $this->levelInterest == PHP_INT_MAX ) {
+            $path = $this->getPath();
+            foreach ( array_keys($this->dataMatch) as $pathReg )    {
+                // Check if matches regex, if this is an array, then check
+                // if matches with regex/0 when looking at first element.
+                if ( preg_match('#^'.$pathReg.'$#', $path ) ||
+                        ( preg_match('#^'.$pathReg.'/0$#', $path ) &&
+                            $this->pathIndex[$this->pathLength-1] == 0 ) ) {
+                    $this->levelInterest = $this->pathLength;
+                }
+            }
+        }
+        
+        // Set object flag (Only when processing objects and not arrays).
+        $this->objectEncountered = ($index === RegexListener::OBJECT);
+    }
+
+    public function endObject(): void    {
+        $obj = array_pop($this->stack);
+        if ( $this->pathLength > 0 )    {
+            $this->label = array_pop($this->path);
+            $this->value($obj);
+            array_pop($this->pathIndex);
+            $this->pathLength--;
+        }
+        else    {
+            $this->value($obj);
+        }
+        // Update array index if neccessary.
+        if ( $this->pathLength > 0 && 
+                    $this->pathIndex[$this->pathLength-1] != RegexListener::OBJECT) {
+            $this->pathIndex[$this->pathLength-1]++;
+        }
+    }
+    
+    public function startArray(): void    {
+        $this->startObject( RegexListener::ARRAY );
+    }
+
+    public function endArray(): void    {
+        $this->endObject();
+    }
+
+    public function whitespace($whitespace): void    {}
+
+    public function startDocument(): void    {
+        $this->complete = false;
+    }
+
+    public function endDocument(): void    {
+        $this->complete = true;
+    }
+
+    public function value($value): void    {
+        $obj = array_pop( $this->stack );
+        $path = $this->getPath();
+           
+        // Check the path against the required matches
+        foreach ( $this->dataMatch as $pathReg => $closure )    {
+            $matches = [];
+            if ( preg_match( '#^'.$pathReg.'$#', $path, $matches ) ) {
+                $closure( $value, $matches[1] ?? [] );
+            }
+        }
+        
+        if ( $this->label != null) {
+            $obj[$this->label] = $value;
+            $this->label = null;
+        } else {
+            $obj[] = $value;
+        }
+        
+        // Check if need to store the data
+        if ( $this->levelInterest <= $this->pathLength )   {
+            $this->stack[] = $obj;
+        }
+        else    {
+            $this->levelInterest = PHP_INT_MAX;
+        }
+        
+        // Update array index if neccessary.
+        if ( $this->objectEncountered == false && 
+                    $this->pathLength > 0 && 
+                    $this->pathIndex[$this->pathLength-1] != RegexListener::OBJECT ) {
+            $this->pathIndex[$this->pathLength-1]++;
+        }
+        $this->objectEncountered = false;
+    }
+
+    public function key($key): void   {
+        $this->label = $key;
+    }    
+    
+    /**
+     * Returns the path at the current element.  This is made up
+     * of both the path (including array indicies) to this point
+     *  as well as the current label(if set).
+     * @return string
+     */
+    private function getPath(): string  {
+        $path = "";
+        foreach ( $this->path as $key => $pathElement)  {
+            if ( !empty ( $pathElement ))  {
+                $path .= "/".$pathElement;
+            }
+            // Add in array index if present
+            if ( $this->pathIndex[$key] != RegexListener::OBJECT )  {
+                $path .= "/".$this->pathIndex[$key];
+            }
+        }
+        // Add in current label if present
+        if ( $this->label != null ) {
+            $path .= "/".$this->label;
+        }
+ 
+        return $path;
+    }
+    
+}
---
+++ tests/RegexListenerTest.php
@@ -0,0 +1,318 @@
+<?php
+
+namespace JsonStreamingParser\Test;
+
+use JsonStreamingParser\Parser;
+use JsonStreamingParser\Listener\RegexListener;
+use PHPUnit\Framework\TestCase;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+class RegexListenerTest extends TestCase {
+    /**
+     * Test reading an attribute as well as a nested object
+     * with a regex symbol.
+     */
+    public function testBasicRead ()    {
+        $filename = __DIR__.'/data/example.json';
+        $testJSON = json_decode(file_get_contents($filename), true);
+        
+        $tssnCall = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['__invoke'])
+            ->getMock();
+        
+        $tssnCall->expects($this->once())
+            ->method('__invoke')
+            ->with($testJSON[0]["totally sweet scientific notation"]);
+
+        $nested_objectCall = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['__invoke'])
+            ->getMock();
+            
+        $nested_objectCall->expects($this->once())
+            ->method('__invoke')
+            ->with($testJSON[1]["nested object"]["nested object?"]);
+
+        $listener = new RegexListener(["/0/totally sweet scientific notation" => $tssnCall,
+            "/1/nested object/nested object\?" => $nested_objectCall]);
+        $parser = new Parser(fopen($filename, 'rb'), $listener);
+        $parser->parse();
+    }
+ 
+    /**
+     * Test reading first and last elements in JSON.
+     */
+    public function testReadFirstAndLast ()    {
+        $filename = __DIR__.'/data/plain.json';
+        $testJSON = json_decode(file_get_contents($filename), true);
+        
+        $firstCall = $this->getMockBuilder(\stdClass::class)
+        ->setMethods(['__invoke'])
+        ->getMock();
+        
+        $firstCall->expects($this->once())
+        ->method('__invoke')
+        ->with($testJSON["key 1"]);
+        
+        $lastCall = $this->getMockBuilder(\stdClass::class)
+        ->setMethods(['__invoke'])
+        ->getMock();
+        
+        $lastCall->expects($this->once())
+        ->method('__invoke')
+        ->with($testJSON["key 3"]);
+        
+        $listener = new RegexListener(["/key 1" => $firstCall,
+            "/key 3" => $lastCall]);
+        $parser = new Parser(fopen($filename, 'rb'), $listener);
+        $parser->parse();
+    }
+    
+    /**
+     * Test reading a nested array of simple data types (integers).
+     */
+    public function testBasicReadArray ()    {
+        $filename = __DIR__.'/data/example.json';
+        $testJSON = json_decode(file_get_contents($filename), true);
+        $arrayData = $testJSON[1]["nested object"]["nested array"];
+        
+        $stream = fopen($filename, "r" );
+        $this->assertNotNull($stream);
+        
+        $arrayCall = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['__invoke'])
+            ->getMock();
+        
+        $arrayList = array_map(function ( $data ) { return [$data]; },
+                $arrayData);
+        $arrayCall->expects($this->exactly(count($arrayData)))
+            ->method('__invoke')
+            ->withConsecutive(...$arrayList);
+            
+        $listener = new RegexListener(["/1/nested object/nested array/\d*" => $arrayCall]);
+        $parser = new Parser(fopen($filename, 'rb'), $listener);
+        $parser->parse();
+    }
+ 
+    /**
+     * Test reading a nested array with objects.
+     */
+    public function testBasicReadArrayElement ()    {
+        $filename = __DIR__.'/data/dateRanges.json';
+        $testJSON = json_decode(file_get_contents($filename), true);
+        $arrayData = array_column($testJSON["data"][0]["rows"],"startDate");
+        $arrayData2 = array_column($testJSON["data"][0]["rows"],"endDate");
+        $arrayData3 = $testJSON["anotherPlace"];
+        
+        $stream = fopen($filename, "r" );
+        $this->assertNotNull($stream);
+        
+        $arrayCall = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['__invoke'])
+            ->getMock();
+        
+        $arrayList = array_map(function ( $data ) { return [$data]; },
+            $arrayData);
+        $arrayCall->expects($this->exactly(count($arrayData)))
+            ->method('__invoke')
+            ->withConsecutive(...$arrayList);
+        
+        $arrayCall2 = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['__invoke'])
+            ->getMock();
+            
+        $arrayList2 = array_map(function ( $data ) { return [$data]; },
+            $arrayData2);
+        $arrayCall2->expects($this->exactly(count($arrayData2)))
+            ->method('__invoke')
+            ->withConsecutive(...$arrayList2);
+            
+        $nested_objectCall = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['__invoke'])
+            ->getMock();
+            
+        $nested_objectCall->expects($this->once())
+            ->method('__invoke')
+            ->with($arrayData3);
+            
+        $listener = new RegexListener(["/data/0/rows/\d*/startDate" => $arrayCall,
+            "/data/0/rows/\d*/endDate" => $arrayCall2,
+            "/anotherPlace" => $nested_objectCall
+        ]);
+        $parser = new Parser(fopen($filename, 'rb'), $listener);
+        $parser->parse();
+    }
+    
+    /**
+     * Test reading root level nested objects
+     */
+    public function testBasicReadArrayObjects ()    {
+        $filename = __DIR__.'/data/example.json';
+        $testJSON = json_decode(file_get_contents($filename), true);
+        
+        $arrayCall = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['__invoke'])
+            ->getMock();
+        
+        $arrayList = array_map(function ( $data ) { return [$data]; },
+                 $testJSON);
+        $arrayCall->expects($this->exactly(count($testJSON)))
+            ->method('__invoke')
+            ->withConsecutive(...$arrayList);
+            
+        $listener = new RegexListener(["/\d*" => $arrayCall]);
+        $parser = new Parser(fopen($filename, 'rb'), $listener);
+        $parser->parse();
+    }
+    
+    /**
+     * Test large file list of ID's
+     */
+    public function testBasicReadArrayIDs ()    {
+        $filename = __DIR__.'/data/ratherBig.json';
+        $testJSON = json_decode(file_get_contents($filename), true);
+        
+        $arrayCall = $this->getMockBuilder(\stdClass::class)
+        ->setMethods(['__invoke'])
+        ->getMock();
+        
+        $arrayList = array_map(function ( $data ) { return [$data]; },
+            array_column($testJSON['rows'],'id'));
+        $arrayCall->expects($this->exactly(count($testJSON['rows'])))
+            ->method('__invoke')
+            ->withConsecutive(...$arrayList);
+        
+        $listener = new RegexListener(["/rows/\d*/id" => $arrayCall]);
+        $parser = new Parser(fopen($filename, 'rb'), $listener);
+        $parser->parse();
+    }
+    
+    
+    /**
+     * Testing the capture group in the JSON path is passed to callback.
+     * As the example file has an 'endDate' element in various elements,
+     * the path of /(.*)/endDate should result in both the element
+     * value as well as the base element name being passed into the callback.
+     */
+    public function testCaptureGroup ()    {
+        $filename = __DIR__.'/data/dateRanges.json';
+        
+        $arrayCall = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['__invoke'])
+            ->getMock();
+        
+        $arrayCall->expects($this->exactly(3))
+            ->method('__invoke')
+            ->withConsecutive(["2013-10-25","/data/0/rows/0"],
+                ["2013-10-27","/data/0/rows/1"],
+                ["2013-11-10","/anotherPlace"]);
+        
+        $listener = new RegexListener(["(.*)/endDate" => $arrayCall]);
+        $parser = new Parser(fopen($filename, 'rb'), $listener);
+        $parser->parse();
+    }
+
+    /**
+     * Testing ability to stop load.
+     */
+    public function testBasicReadStop ()    {
+        $filename = __DIR__.'/data/example.json';
+        $testJSON = json_decode(file_get_contents($filename), true);
+        
+        $arrayCall = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['__invoke'])
+            ->getMock();
+        
+        $arrayCall->expects($this->once())
+            ->method('__invoke')
+            ->with($testJSON[0]['name']);;
+ 
+        $notCall = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['__invoke'])
+            ->getMock();
+            
+        $notCall->expects($this->never())
+            ->method('__invoke')
+            ->with($testJSON[0]['zero character']);;
+            
+        $listener = new RegexListener();
+        $parser = new Parser(fopen($filename, 'rb'), $listener);
+        $listener->setMatch(["/0/name" => $arrayCall,
+            "/0/integer" => function($data) use ($parser) {
+                $parser->stop();
+            },
+            "/0/zero character" => $notCall
+        ]);
+        $parser->parse();
+    }
+    
+    /**
+     * Test reading a corrupt file.
+     * Extracts the type element of each of the features.
+     */
+    public function testCorruptFile ()    {
+        $filename = __DIR__.'/data/example.corrupted.json';
+        
+        $arrayCall = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['__invoke'])
+            ->getMock();
+        
+        $arrayCall->expects($this->exactly(2))
+            ->method('__invoke')
+            ->withConsecutive(["Point"], ["Polygon"]);
+        
+        $listener = new RegexListener(["/features/\d*/geometry/type" => $arrayCall]);
+        $parser = new Parser(fopen($filename, 'rb'), $listener);
+        $parser->parse();
+    }
+    
+    /**
+     * Test reading a simple array of integers.
+     */
+    public function testBasicArrayRead ()    {
+        $filename = __DIR__.'/data/simpleArray.json';
+        $testJSON = json_decode(file_get_contents($filename), true);
+        
+        $value1Call = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['__invoke'])
+            ->getMock();
+        
+        $value1Call->expects($this->once())
+            ->method('__invoke')
+            ->with($testJSON[0]);
+        
+        $value2Call = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['__invoke'])
+            ->getMock();
+        
+        $value2Call->expects($this->once())
+            ->method('__invoke')
+            ->with($testJSON[1]);
+        
+        $listener = new RegexListener(["/0" => $value1Call,
+            "/1" => $value2Call]);
+        $parser = new Parser(fopen($filename, 'rb'), $listener);
+        $parser->parse();
+    }
+    
+    /**
+     * Test reading a simple array of integers with capture group.
+     */
+    public function testBasicArrayReadCapture ()    {
+        $filename = __DIR__.'/data/simpleArray.json';
+        
+        $valueCall = $this->getMockBuilder(\stdClass::class)
+            ->setMethods(['__invoke'])
+            ->getMock();
+        
+        $valueCall->expects($this->exactly(2))
+            ->method('__invoke')
+            ->withConsecutive(["1","/0"],
+                ["2","/1"]);
+        
+        $listener = new RegexListener(["(/\d*)" => $valueCall]);
+        $parser = new Parser(fopen($filename, 'rb'), $listener);
+        $parser->parse();
+    }
+}
